cc19328596261edf22c857fe2a0912d3afbcf3b2
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-28 14:37:44 +0200

    set an array of users's data

---
+++ usersdb.php
@@ -0,0 +1,20 @@
+<?php
+    $users = [
+        [
+            'name' => 'Mario',
+            'email' => 'mario@gmail.com',
+            'password' => '123456'
+        ],
+        [
+            'name' => 'Luigi',
+            'email' => 'luigi@gmail.com',
+            'password' => 'asdf'
+        ],
+        [
+            'name' => 'Peach',
+            'email' => 'peach@gmail.com',
+            'password' => 'asdf1'
+        ],
+    ];
+
+?>
