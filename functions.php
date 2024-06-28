19cffb36309b1386e25a2675756e7b97f846a443
Piccio <lorenzo.hl.picchi@gmail.com>
2024-06-28 15:56:30 +0200

    set a function to validate login

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php 
+require __DIR__ . '/usersdb.php';
+
+
+
+/**
+ * 
+ * 
+ *Login function to validate username and password
+ * 
+ */
+
+function login($username, $password, $users) {
+    foreach ( $users as $user) {
+        if ($user['username'] === $username && $user['password'] === $password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+?>
